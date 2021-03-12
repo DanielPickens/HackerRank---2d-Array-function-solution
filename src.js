943c47c36a4df8b42ed19d725ad5c1daa5027297
Daniel Pickens <72703981+DanielPickens@users.noreply.github.com>
2021-03-11 19:28:36 -0700

    Update src.js

--- src.js
+++ src.js
@@ -27,3 +27,24 @@ function hourGlass(arr) {
 
 
 time complx: O(n*m)
+
+
+
+2nd solution: 
+
+function hourGlass(arr) {
+    let max = -63;
+
+    for (let i = 0; i < 4; i++) {
+        for (let j = 0; j < 4; j++) {
+            let sum = arr[i + 1][j + 1];
+            for (let k = 0; k < 3; k++) {
+                sum += arr[i][j + k];
+                sum += arr[i + 2][j + k]
+            }
+            if (sum > max) max = sum;
+        }
+    }
+
+    return max;
+}
